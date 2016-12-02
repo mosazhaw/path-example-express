6454e8877c3185a0bf7415e007314b94f50ed06c
Adrian Moser <trekking09@gmail.com>
2016-12-02 11:03:17 +0100

    Implement a sample app #1
    use loki ids

--- server/database.ts
+++ server/database.ts
@@ -7,10 +7,10 @@ export class Database {
     constructor() {
         this._db = new this._loki('PathExample');
         this._person = this._db.addCollection('person');
-        this._person.insert({key: 1, firstName:'Adam', familyName: 'Jones'});
-        this._person.insert({key: 2, firstName:'Betty', familyName: 'Miller'});
-        this._person.insert({key: 3, firstName:'Chris', familyName: 'Connor'});
-        this._person.insert({key: 4, firstName:'Dave', familyName: 'Dean'});
+        this._person.insert({firstName:'Adam', familyName: 'Jones'});
+        this._person.insert({firstName:'Betty', familyName: 'Miller'});
+        this._person.insert({firstName:'Chris', familyName: 'Connor'});
+        this._person.insert({firstName:'Dave', familyName: 'Dean'});
     }
 
     public getPersons() : any {
@@ -18,11 +18,12 @@ export class Database {
         for (let person of this._person.find()) {
             let entry:PathListEntry = new PathListEntry();
             let key:PathListKey = new PathListKey();
-            key.key = person.key;
+            console.log(person);
+            key.key = person['$loki'];
             key.name = "personKey";
             entry.key = key;
             entry.name = person.familyName + ' ' + person.firstName;
-            entry.details.push('' + person.key); // must be string
+            entry.details.push('' + person['$loki']); // must be string
             result.push(entry);
         }
         return result;
@@ -35,12 +36,12 @@ export class Database {
 
     public getPerson(personKey:number) : any {
         let query:any = {};
-        query["key"] = personKey;
+        query["$loki"] = personKey;
         let result:any = this._person.findOne(query);
         result = JSON.parse(JSON.stringify(result)); // clone
 
         let key:PathListKey = new PathListKey();
-        key.key = result.key;
+        key.key = result.id;
         key.name = "personKey";
         result.key = key;
         return result;
@@ -48,7 +49,7 @@ export class Database {
 
     public updatePerson(personKey:number, data:any) : boolean {
         let query:any = {};
-        query["key"] = personKey;
+        query["$loki"] = personKey;
         let person:any = this._person.findOne(query);
         person.firstName = data.firstName;
         person.familyName = data.familyName;
@@ -58,7 +59,7 @@ export class Database {
 
     public deletePerson(personKey:number) {
         let query:any = {};
-        query["key"] = personKey;
+        query["$loki"] = personKey;
         let person:any = this._person.findOne(query);
         this._person.remove(person);
         return true;
