2afa5bd5c13c2b21898ae4ba6daebed5eaf9b844
Adrian Moser <trekking09@gmail.com>
2016-12-20 16:27:59 +0100

    show hobbies for contacts

--- server/rest/person-rest-service.ts
+++ server/rest/person-rest-service.ts
@@ -1,6 +1,8 @@
 import {AbstractRestService} from "./abstract-rest-service";
 import {AbstractDatabase} from "../database/abstract-database";
 import {HobbyDatabase} from "../database/hobby-database";
+import {PathListEntry} from "../data/path-list-entry";
+import {PathListKey} from "../data/path-list-key";
 
 export class PersonRestService extends AbstractRestService {
 
@@ -30,7 +32,21 @@ export class PersonRestService extends AbstractRestService {
         this._app.get('/services/person/:personKey/hobby', (req, res) => {
             service.hobbyDatabase.list().then((rows) => {
                 rows.pop();
-                service.hobbyDatabase.createPathList(rows, res);
+                var promises = [];
+                for (let hobby of rows) {
+                    // TODO create generic method
+                    let entry = new PathListEntry();
+                    let key: PathListKey = new PathListKey();
+                    key.key = hobby.id;
+                    key.name = service.hobbyDatabase.getEntityName() + "Key";
+                    entry.key = key;
+                    promises.push(service.hobbyDatabase.createPathListEntry(entry, hobby["doc"]));
+                }
+                Promise.all(promises).then((result) => {
+                    res.json(result);
+                }).catch((err) => {
+                    console.log(err);
+                })
             }).catch((err) => {
                 console.log(err);
             })
