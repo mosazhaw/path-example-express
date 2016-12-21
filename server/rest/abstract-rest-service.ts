b88d6201bc28dff450d349155bec0e2608e74961
Adrian Moser <trekking09@gmail.com>
2016-12-21 09:06:53 +0100

    refactor to use async / await, upgrade node/npm

--- server/rest/abstract-rest-service.ts
+++ server/rest/abstract-rest-service.ts
@@ -15,55 +15,40 @@ export abstract class AbstractRestService {
 
     protected initList() {
         let service = this;
-        this._app.get('/services/' + service._database.getEntityName() + '', (req, res) => {
-            service._database.list().then((rows) => {
-                this._database.createPathList(rows, res);
-            }).catch((err) => {
-                console.log(err);
-            })
+        this._app.get('/services/' + service._database.getEntityName() + '', async (req, res) => {
+            let rows = await service._database.list();
+            this._database.createPathList(rows, res);
         });
     }
 
     protected initCreate() {
-        this._app.post('/services/' + this._database.getEntityName() + '', (req, res) => {
-            this._database.create(req.body).then((newDoc) => {
-                res.json(newDoc);
-            }).catch((err) => {
-                console.log(err);
-            });
+        this._app.post('/services/' + this._database.getEntityName() + '', async (req, res) => {
+            let newDoc = await this._database.create(req.body);
+            res.json(newDoc);
         });
     }
 
     protected initRead() {
-        this._app.get('/services/' + this._database.getEntityName() + '/:key', (req, res) => {
+        this._app.get('/services/' + this._database.getEntityName() + '/:key', async (req, res) => {
             let key: string = req.params.key;
-            this._database.read(key).then((doc) => {
-                res.json(doc);
-            }).catch((err) => {
-                console.log(err);
-            });
+            let doc = await this._database.read(key);
+            res.json(doc);
         });
     }
 
     protected initUpdate() {
-        this._app.put('/services/' + this._database.getEntityName() + '/:key', (req, res) => {
+        this._app.put('/services/' + this._database.getEntityName() + '/:key', async (req, res) => {
             let key: string = req.params.key;
-            this._database.update(key, req.body).then((doc) => {
-                res.json(doc);
-            }).catch((err) => {
-                console.log(err);
-            });
+            let doc = await this._database.update(key, req.body);
+            res.json(doc);
         });
     }
 
     protected initDelete() {
-        this._app.delete('/services/' + this._database.getEntityName() + '/:key', (req, res) => {
+        this._app.delete('/services/' + this._database.getEntityName() + '/:key', async (req, res) => {
             let key: string = req.params.key;
-            this._database.delete(key).then((doc) => {
-                res.json({message: 'deleted'});
-            }).catch((err) => {
-                console.log(err);
-            });
+            let doc = await this._database.delete(key);
+            res.json({message: 'deleted'});
         });
     }
 }
