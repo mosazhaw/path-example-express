431cc08635305b2e69fded44ae68811b6e93a0d4
Adrian Moser <trekking09@gmail.com>
2017-03-16 08:20:01 +0100

    prepare for heroku

--- server/server.ts
+++ server/server.ts
@@ -37,7 +37,7 @@ app.all('/*',function(req,res,next){
 app.options("/*", function(req, res, next){
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
+    res.sendStatus(200);
 });
 
 // disable Caching
