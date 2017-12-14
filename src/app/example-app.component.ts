a375b66f6167fbe754424107fd2cdd3eb005192c
Adrian Moser <trekking09@gmail.com>
2017-12-14 15:35:50 +0100

    update to path v0.2.12

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -23,7 +23,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.2.8";
+        return "0.2.12";
     }
 
     protected getStartPage():string {
