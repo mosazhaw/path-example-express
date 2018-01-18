e952f22c0369bfdf179307e4fe8d0d60d01088f1
Adrian Moser <trekking09@gmail.com>
2018-01-18 16:07:58 +0100

    update to v0.2.14

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -23,7 +23,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.2.13";
+        return "0.2.14";
     }
 
     protected getStartPage():string {
