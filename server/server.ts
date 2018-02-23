578cf511d0b9e638f40fa17ee4c9b604247cc3e3
Adrian Moser <trekking09@gmail.com>
2018-02-23 11:26:00 +0100

    update to path v0.2.15

--- server/server.ts
+++ server/server.ts
@@ -50,7 +50,7 @@ app.get('/*',function(req,res,next) {
 
 // Path ping request
 app.get('/services/ping', function(req, res) {
-    res.json({ status: 'ok', userId : 'demo', version: '0.2.14' });
+    res.json({ status: 'ok', userId : 'demo', version: '0.2.15' });
 });
 
 // Path example entities
--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -23,7 +23,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.2.14";
+        return "0.2.15";
     }
 
     protected getStartPage():string {
