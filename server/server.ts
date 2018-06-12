d7cb06225ca696882b5ba6c7eadfe92a4e8176a4
Adrian Moser <trekking09@gmail.com>
2018-06-12 12:14:56 +0200

    set version v0.3.0

--- server/server.ts
+++ server/server.ts
@@ -50,7 +50,7 @@ app.get('/*',function(req,res,next) {
 
 // Path ping request
 app.get('/services/ping', function(req, res) {
-    res.json({ status: 'ok', userId : 'demo', version: '0.2.23' });
+    res.json({ status: 'ok', userId : 'demo', version: '0.3.0' });
 });
 
 // Path example entities
