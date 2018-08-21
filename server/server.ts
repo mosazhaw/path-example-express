746dee9c8a5091a891395170125d1f864444bea8
innovad <mosa@zhaw.ch>
2018-08-21 15:49:42 +0200

    set version v0.3.6

--- server/server.ts
+++ server/server.ts
@@ -50,7 +50,7 @@ app.get('/*',function(req,res,next) {
 
 // Path ping request
 app.get('/services/ping', function(req, res) {
-    res.json({ status: 'ok', userId : 'demo', version: '0.3.5' });
+    res.json({ status: 'ok', userId : 'demo', version: '0.3.6' });
 });
 
 // Path example entities
--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -25,7 +25,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.3.5";
+        return "0.3.6";
     }
 
     protected getStartPage():string {
