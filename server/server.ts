a2d26b5016d66da8e5fa0132978d0cfac6f5fb82
innovad <mosa@zhaw.ch>
2018-09-12 16:42:14 +0200

    set version v0.3.12

--- server/server.ts
+++ server/server.ts
@@ -51,7 +51,7 @@ app.get('/*',function(req,res,next) {
 
 // Path ping request
 app.get('/services/ping', function(req, res) {
-    res.json({ status: 'ok', userId : 'demo', version: '0.3.8' });
+    res.json({ status: 'ok', userId : 'demo', version: '0.3.12' });
 });
 
 // Path example entities
