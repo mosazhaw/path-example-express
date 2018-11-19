7c0bfecdb3104d45cbdb88eee2c8af966bd24ed8
innovad <mosa@zhaw.ch>
2018-11-19 11:35:15 +0100

    adapt imports to 0.5.0

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -1,26 +1,27 @@
 /* angular/path imports */
 import {Component, Type} from "@angular/core";
-import * as path from "path-framework/app/path-framework/path";
+import {PathAppComponent} from "path-framework/app/path-framework/path-app.component";
+import {PathService} from "path-framework/app/path-framework/service/path.service";
+import {TranslationService} from "path-framework/app/path-framework/service/translation.service";
+import {CustomPageElement} from "path-framework/app/path-framework/page/element/custom/custom-container.component";
 
 /* model imports */
-import {GuiModel} from "./gui-model/guimodel";
 import * as handler from "./gui-model/form/handlers";
 import * as beans from "./gui-model/generated/forms";
-import {TranslationService} from "path-framework/app/path-framework/service/translation.service";
+import {GuiModel} from "./gui-model/guimodel";
 import {ExampleTranslationService} from "./example-translation-service";
 import {ExampleComponent} from "./custom/example.component";
-import {CustomPageElement} from "path-framework/app/path-framework/page/element/custom/custom-container.component";
 
 @Component({
     selector: "path-application",
     templateUrl: "./../../node_modules/path-framework/app/path-framework/path-app.component.html",
-    providers: [path.PathService, {provide: TranslationService, useClass: ExampleTranslationService}]
+    providers: [PathService, {provide: TranslationService, useClass: ExampleTranslationService}]
 })
-export class ExampleAppComponent extends path.PathAppComponent {
+export class ExampleAppComponent extends PathAppComponent {
 
     private _appConfig = new GuiModel();
 
-    constructor(pathService: path.PathService, translationService: TranslationService) {
+    constructor(pathService: PathService, translationService: TranslationService) {
         super(pathService, translationService);
     }
 
