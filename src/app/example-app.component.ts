3c845d2e6148bec48d755a341968f034657120ae
innovad <mosa@zhaw.ch>
2018-11-19 11:20:36 +0100

    use path version 0.5.0

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -25,7 +25,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion(): string {
-        return "0.4.5";
+        return "0.5.0";
     }
 
     protected getStartPage(): string {
