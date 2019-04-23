a63c54aa688cce410c1fb21abda675facdfaa879
innovad <mosa@zhaw.ch>
2019-04-23 10:51:26 +0200

    use path version v0.5.5

--- server/server.ts
+++ server/server.ts
@@ -57,7 +57,7 @@ app.get("/*", function (req, res, next) {
 
 // Path ping request
 app.get("/services/ping", function (req, res) {
-    res.json({status: "ok", userId: "demo", version: "0.5.4"});
+    res.json({status: "ok", userId: "demo", version: "0.5.5"});
 });
 
 // Path example entities
